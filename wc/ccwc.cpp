da96ed2dfa161cd470281837778fad678d13b991
Apolo151 <abdogofor55@gmail.com>
2023-12-03 15:30:34 +0200

    feat: read from stdin if no file is specified

--- wc/ccwc.cpp
+++ wc/ccwc.cpp
@@ -39,30 +39,63 @@ wc calc(ifstream &fin){
     return res;
 }
 
+wc calc(istream &cin){
+    /*return the no. of bytes in the file*/
+    /*char size = 1 byte*/
+    wc res;
+    char c;
+    bool state=false; // false means empty space, true means a word char
+    while(cin.get(c)){
+        ++res.chars;
+        if(c=='\n'){
+            res.lines++;
+        }
+        if(c == ' ' || c == '\t' || c == '\n' || c == '\r'){
+            state=false;
+        }
+        else{
+            if(!state)
+                res.words++;
+            state=true;
+        }
+    }
+    return res;
+}
+
+bool is_param(string s){
+    if(s=="-c" || s=="-m" || s=="-l" || s=="-w")
+        return true;
+    return false;
+}
+
 
 int main(int argc, char *argv[]){
     ifstream fin; // create filestream object
-    fin.open(argv[argc-1]);
-    wc res = calc(fin); 
-    //cout << (strcmp(argv[1], "-c"));
-    //cout << res.lines << endl;
-    string ss;
-    if(argc > 2){
-        for(int i=1; i < argc; i++){
-            ss = argv[i];
-            if(ss=="-l")
-                cout << res.lines << " ";
-            else if(ss=="-c" || ss=="-m")
-                cout << res.chars << " ";
-            else if(ss=="-w")
-                cout << res.words << " ";
-        }
-    }
-    else if(argc > 1){
-       cout << res.toString() << " " << argv[argc-1]; 
+    string filename = argv[argc-1];
+    wc res;
+    //
+    bool no_file = is_param(filename);
+    if(no_file){
+        res = calc(cin);
+        filename = "";
     }
     else{
-        cout << "Please specify your file name";
+        fin.open(filename);
+        res = calc(fin);
+    }
+    string ss;
+    for(int i=1; i < argc; i++){
+        ss = argv[i];
+        if(ss=="-l")
+            cout << res.lines << " ";
+        else if(ss=="-c" || ss=="-m")
+            cout << res.chars << " ";
+        else if(ss=="-w")
+            cout << res.words << " ";
+    }
+    //
+    if((argc==1 && no_file) || (argc==2 && !no_file)){
+       cout << res.toString() << " " << filename; 
     }
 
     fin.close();
